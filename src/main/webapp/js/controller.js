5b9b061868c870869ed23f60be5ed6fbcc02bf1d
harshana <jmhmjayamaha@gmail.com>
2017-04-18 22:59:13 +0530

    add student form

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -2,30 +2,21 @@ app.controller("studentController", function($scope, $http) {
 	
 	$scope.addStudent = function() {
 		var dataObject = {
-			/*"stuNo" : $scope.stuNo,
+			"stuNo" : $scope.stuNo,
 			"firstName" : $scope.fName,
 			"lastName" : $scope.lName,
 			"gender" : $scope.gender,
 			"age" : $scope.age,
 			"address" : $scope.address,
-			"email" : $scope.email*/
-				
-				"stuNo" : "seu1",
-				"firstName" : "harshana",
-				"lastName" : "madusanka",
-				"gender" : "male",
-				"age" : $scope.age,
-				"address" : "dorawaka",
-				"email" : "harshanaj@wso2.com"
+			"email" : $scope.email			
 		}
 	
-		dataObject.age = $scope.age;
 		$http.post("http://localhost:8080/student-app/api/student",dataObject).then(
 			function() {
 				alert("sucessfully added");
 			} , 
 			function() {
-				alert(dataObject.age);
+				alert("something went wrong");
 			}
 		);
 	}
