6f76a12aa499a95165848a1182ebe71a922c58a8
harshana <jmhmjayamaha@gmail.com>
2017-04-19 22:35:31 +0530

    update student form is added

--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -20,4 +20,45 @@ app.controller("studentController", function($scope, $http) {
 			}
 		);
 	}
+
+	$scope.findStudent = function() {
+		
+		$http.get("http://localhost:8080/student-app/api/student/" + $scope.search).then(
+			function(response) {
+				$scope.student =  response.data;	
+				
+				$scope.stuNo = $scope.student.stuNo;
+				$scope.fName = $scope.student.firstName;
+				$scope.lName = $scope.student.lastName;
+				$scope.gender = $scope.student.gender;
+				$scope.age = $scope.student.age;
+				$scope.address = $scope.student.address;
+				$scope.email = $scope.student.email;
+			} ,
+			function() {
+				alert("Student number is invalid");
+			}
+		);
+	}
+	
+	$scope.updateStudent = function() {
+		var dataObject = {
+			"stuNo" : $scope.stuNo,
+			"firstName" : $scope.fName,
+			"lastName" : $scope.lName,
+			"gender" : $scope.gender,
+			"age" : $scope.age,
+			"address" : $scope.address,
+			"email" : $scope.email
+		}
+		
+		$http.put("http://localhost:8080/student-app/api/student/"+ $scope.stuNo,  dataObject).then(
+			function() {
+				alert("successfully updated");
+			} , 
+			function() {
+				alert("something went wrong");
+			}
+		);
+	}
 });
